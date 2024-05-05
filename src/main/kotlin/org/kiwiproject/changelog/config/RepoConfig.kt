afcc5b38f883c2012911adf8799c8523b2cf05c4
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-05-04 21:18:11 -0400

    Cleanup after switch to picocli for CLI argument handling
    * Remove default for defaultCategory in CategoryConfig
    * Remove defaults for constructor args in RepoConfig
    * Remove obsolete code: main.kt and CommandLineArgs.kt
    * Remove kotlinx-cli-jvm dependency
    
    Relates to #129

--- src/main/kotlin/org/kiwiproject/changelog/config/RepoConfig.kt
+++ src/main/kotlin/org/kiwiproject/changelog/config/RepoConfig.kt
@@ -2,7 +2,6 @@ package org.kiwiproject.changelog.config
 
 import java.io.File
 
-// TODO Remove the default values; these should be required here without default values
-data class RepoConfig(val workingDir: File = File("."),
-                      val previousRevision: String = "main",
-                      val revision: String = "HEAD")
+data class RepoConfig(val workingDir: File,
+                      val previousRevision: String,
+                      val revision: String)
