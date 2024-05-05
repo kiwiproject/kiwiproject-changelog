958b56f2e843fc4a8809ef05b3674c80df9e2083
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-05-04 21:19:30 -0400

    Cleanup after switch to picocli for CLI argument handling (#133)
    * Remove default for defaultCategory in CategoryConfig
    * Remove defaults for constructor args in RepoConfig
    * Remove obsolete code: main.kt and CommandLineArgs.kt
    * Remove kotlinx-cli-jvm dependency
    
    Relates to #129

--- src/main/kotlin/org/kiwiproject/changelog/config/CategoryConfig.kt
+++ src/main/kotlin/org/kiwiproject/changelog/config/CategoryConfig.kt
@@ -1,7 +1,6 @@
 package org.kiwiproject.changelog.config
 
-// TODO Probably should remove the default for defaultCategory; should also make args non-nullable
-data class CategoryConfig(val defaultCategory: String = "Assorted",
+data class CategoryConfig(val defaultCategory: String,
                           val labelToCategoryMapping: Map<String, String>?,
                           val alwaysIncludePRsFrom: List<String>?,
                           val categoryOrder: List<String>?)
