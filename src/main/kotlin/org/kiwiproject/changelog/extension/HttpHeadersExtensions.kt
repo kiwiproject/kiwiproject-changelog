cfe0e79be62adf791df994e46566704d842d8bdd
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-10-13 11:09:19 -0400

    Make exceptions consistent in HttpHeadersExtensions
    * Change firstValueAsLongOrThrow to throw IllegalStateException
      instead of NumberFormatException if the header exists but
      does not parse as a Long value. The NumberFormatException
      is now the cause of the IllegalStateException.
    
    Closes #244

--- src/main/kotlin/org/kiwiproject/changelog/extension/HttpHeadersExtensions.kt
+++ src/main/kotlin/org/kiwiproject/changelog/extension/HttpHeadersExtensions.kt
@@ -1,14 +1,21 @@
 package org.kiwiproject.changelog.extension
 
 import java.net.http.HttpHeaders
+import java.util.OptionalLong
 
 fun HttpHeaders.firstValueOrNull(name: String): String? = firstValue(name).orElse(null)
 
 fun HttpHeaders.firstValueOrThrow(name: String): String =
     firstValue(name).orElseThrow { newIllegalStateException(name) }
 
-fun HttpHeaders.firstValueAsLongOrThrow(name: String): Long =
-    firstValueAsLong(name).orElseThrow { newIllegalStateException(name) }
+fun HttpHeaders.firstValueAsLongOrThrow(name: String): Long {
+    val optionalLong: OptionalLong? = try {
+        firstValueAsLong(name)
+    } catch (e: Exception) {
+        throw IllegalStateException("$name header exists, but its value does not parse as a Long", e)
+    }
+    return optionalLong!!.orElseThrow { newIllegalStateException(name) }
+}
 
 private fun newIllegalStateException(name: String): IllegalStateException =
     IllegalStateException("$name header was expected, but does not exist (case-insensitive)")
