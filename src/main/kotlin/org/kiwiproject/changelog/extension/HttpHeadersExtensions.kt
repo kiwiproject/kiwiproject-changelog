70b2bbd8e02891b86632b64b1dd64a6d20fa3475
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-10-13 11:04:51 -0400

    Add better exception messages in HttpHeadersExtensions.kt (#246)
    * Update the exception message
    * Extract private helper function to reduce duplicate code
    * Add a test (the exception cases were not being tested)
    
    Closes #243

--- src/main/kotlin/org/kiwiproject/changelog/extension/HttpHeadersExtensions.kt
+++ src/main/kotlin/org/kiwiproject/changelog/extension/HttpHeadersExtensions.kt
@@ -5,7 +5,10 @@ import java.net.http.HttpHeaders
 fun HttpHeaders.firstValueOrNull(name: String): String? = firstValue(name).orElse(null)
 
 fun HttpHeaders.firstValueOrThrow(name: String): String =
-    firstValue(name).orElseThrow { IllegalStateException("$name header is required") }
+    firstValue(name).orElseThrow { newIllegalStateException(name) }
 
 fun HttpHeaders.firstValueAsLongOrThrow(name: String): Long =
-    firstValueAsLong(name).orElseThrow { IllegalStateException("$name header is required")}
+    firstValueAsLong(name).orElseThrow { newIllegalStateException(name) }
+
+private fun newIllegalStateException(name: String): IllegalStateException =
+    IllegalStateException("$name header was expected, but does not exist (case-insensitive)")
